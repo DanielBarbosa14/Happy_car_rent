0a952744a258d1daa9b8f4367b7d611fd06a6e1a
DanielBarbosa14 <daniel.alves.barbosa@ipvc.pt>
2023-12-11 18:46:32 +0000

    .

--- src/pages/Lojas.tsx
+++ src/pages/Lojas.tsx
@@ -7,55 +7,20 @@ import {
   IonPage,
   IonTitle,
   IonToolbar,
+  IonButton,
+  IonModal,
   IonList,
   IonItem,
   IonLabel,
-  IonButton,
-  IonModal,
-  IonHeader as ModalHeader,
-  IonContent as ModalContent,
-  IonToolbar as ModalToolbar,
-  IonTitle as ModalTitle,
+  IonImg,
 } from '@ionic/react';
-import { useHistory } from 'react-router-dom';
+import { useParams } from 'react-router';
 import ExploreContainer from '../components/ExploreContainer';
 import './Lojas.css';
 
-// Exemplo de lista de lojas
-const lojasData = [
-  {
-    id: 1,
-    nome: 'Viana do Castelo',
-    imagemCidade: 'url_imagem_loja_a',
-    morada: 'Rua do pedreiros',
-    mapa: 'url_mapa_loja_a',
-  },
-  // Adicione mais lojas conforme necessário
-];
-
 const Lojas: React.FC = () => {
-  const [selectedLoja, setSelectedLoja] = useState(null);
+  const { name } = useParams<{ name: string }>();
   const [showModal, setShowModal] = useState(false);
-  const history = useHistory();
-
-  const openModal = (loja) => {
-    setSelectedLoja(loja);
-    setShowModal(true);
-  };
-
-  const closeModal = () => {
-    setSelectedLoja(null);
-    setShowModal(false);
-  };
-
-  const navigateToFrota = () => {
-    if (selectedLoja) {
-      // Aqui você pode navegar para a página da frota e aplicar o filtro necessário
-      // Substitua 'selectedLoja.id' pelo identificador real da loja
-      history.push(`/frota/${selectedLoja.id}`);
-    }
-    closeModal();
-  };
 
   return (
     <IonPage>
@@ -71,39 +36,44 @@ const Lojas: React.FC = () => {
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Lojas</IonTitle>
+            <IonTitle size="large">{name}</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Lojas" />
+        <ExploreContainer name={name} />
 
-        {/* Lista de Lojas */}
         <IonList>
-          {lojasData.map((loja) => (
-            <IonItem key={loja.id} onClick={() => openModal(loja)}>
-              <IonLabel>{loja.nome}</IonLabel>
-            </IonItem>
-          ))}
+          <IonItem>
+            <IonLabel>Viana do castelo</IonLabel>
+            <IonButton onClick={() => setShowModal(true)}>Ver mais</IonButton>
+            <IonModal isOpen={showModal} onDidDismiss={() => setShowModal(false)}>
+          <IonContent>
+            <IonTitle></IonTitle>
+            <IonImg src="https://iili.io/EkXCvt.md.jpg" alt="Descrição da imagem" style={{ width: '100%', height: 'auto' }} />
+            <IonButton onClick={() => setShowModal(false)}>Fechar Modal</IonButton>
+          </IonContent>
+        </IonModal>
+          </IonItem>
+          
+          <IonItem>
+            <IonLabel>Braga</IonLabel>
+            <IonButton onClick={() => setShowModal(true)}>Ver mais </IonButton>
+            <IonModal isOpen={showModal} onDidDismiss={() => setShowModal(false)}>
+          <IonContent>
+            <IonTitle></IonTitle>
+            <IonImg src="https://iili.io/EkXCvt.md.jpg" alt="Descrição da imagem" style={{ width: '100%', height: 'auto' }} />
+            <IonButton onClick={() => setShowModal(false)}>Fechar Modal</IonButton>
+          </IonContent>
+        </IonModal>
+          </IonItem>
+          
         </IonList>
 
-        {/* Modal com informações da loja */}
-        <IonModal isOpen={showModal} onDidDismiss={closeModal}>
-          <ModalHeader>
-            <ModalToolbar>
-              <IonTitle>{selectedLoja?.nome}</IonTitle>
-            </ModalToolbar>
-          </ModalHeader>
-          <ModalContent>
-            {/* Conteúdo da modal */}
-            {selectedLoja && (
-              <div>
-                <img src={selectedLoja.imagemCidade} alt={`Cidade da ${selectedLoja.nome}`} />
-                <p>Morada: {selectedLoja.morada}</p>
-                {/* Adicione aqui um componente de mapa usando selectedLoja.mapa */}
-                <IonButton onClick={navigateToFrota}>Consultar Lista de Carros</IonButton>
-                <IonButton onClick={closeModal}>Fechar</IonButton>
-              </div>
-            )}
-          </ModalContent>
+        <IonModal isOpen={showModal} onDidDismiss={() => setShowModal(false)}>
+          <IonContent>
+            <IonTitle></IonTitle>
+            <IonImg src="https://iili.io/EkXCvt.md.jpg" alt="Descrição da imagem" style={{ width: '100%', height: 'auto' }} />
+            <IonButton onClick={() => setShowModal(false)}>Fechar Modal</IonButton>
+          </IonContent>
         </IonModal>
       </IonContent>
     </IonPage>
