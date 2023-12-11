5390a149031b5c92b01f30091b0a24102e97b093
DanielBarbosa14 <daniel.alves.barbosa@ipvc.pt>
2023-12-11 12:19:24 +0000

    .

---
+++ src/components/Menu.tsx
@@ -0,0 +1,70 @@
+import {
+  IonContent,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonListHeader,
+  IonMenu,
+  IonMenuToggle,
+  IonNote,
+} from '@ionic/react';
+
+import { useLocation } from 'react-router-dom';
+import { archiveOutline, archiveSharp, bookmarkOutline, carSportOutline, heartOutline, heartSharp, home, homeOutline, mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, storefrontOutline, trashOutline, trashSharp, warningOutline, warningSharp } from 'ionicons/icons';
+import './Menu.css';
+
+interface AppPage {
+  url: string;
+  iosIcon: string;
+  mdIcon: string;
+  title: string;
+}
+
+const appPages: AppPage[] = [
+  {
+    title: 'Início',
+    url: '/inicio',
+    iosIcon: homeOutline,
+    mdIcon: homeOutline
+  },
+  {
+    title: 'Lojas',
+    url: '/lojas',
+    iosIcon: storefrontOutline,
+    mdIcon: storefrontOutline
+  },
+  {
+    title: 'Frota',
+    url: '/frota',
+    iosIcon: carSportOutline,
+    mdIcon: carSportOutline
+  },
+];
+
+const Menu: React.FC = () => {
+  const location = useLocation();
+
+  return (
+    <IonMenu contentId="main" type="overlay">
+      <IonContent>
+        <IonList id="inbox-list">
+          <IonListHeader>Happy Car Rent</IonListHeader>
+          <IonNote>Happy Car Rent evolution!</IonNote>
+          {appPages.map((appPage, index) => {
+            return (
+              <IonMenuToggle key={index} autoHide={false}>
+                <IonItem className={location.pathname === appPage.url ? 'selected' : ''} routerLink={appPage.url} routerDirection="none" lines="none" detail={false}>
+                  <IonIcon aria-hidden="true" slot="start" ios={appPage.iosIcon} md={appPage.mdIcon} />
+                  <IonLabel>{appPage.title}</IonLabel>
+                </IonItem>
+              </IonMenuToggle>
+            );
+          })}
+        </IonList>
+      </IonContent>
+    </IonMenu>
+  );
+};
+
+export default Menu;
---
+++ src/pages/Lojas.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import {
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonItem,
+  IonLabel,
+  IonList,
+} from '@ionic/react';
+import { useParams } from 'react-router';
+import ExploreContainer from '../components/ExploreContainer';
+import './Lojas.css';
+
+const Example: React.FC = () => {
+  return (
+    <IonContent color="light">
+      <IonList inset={true}>
+        <IonItem>
+          <IonLabel>Pokémon Yellow</IonLabel>
+        </IonItem>
+        <IonItem>
+          <IonLabel>Mega Man X</IonLabel>
+        </IonItem>
+        <IonItem>
+          <IonLabel>The Legend of Zelda</IonLabel>
+        </IonItem>
+        <IonItem>
+          <IonLabel>Pac-Man</IonLabel>
+        </IonItem>
+        <IonItem>
+          <IonLabel>Super Mario World</IonLabel>
+        </IonItem>
+      </IonList>
+    </IonContent>
+  );
+};
+
+const Lojas: React.FC = () => {
+  const { name } = useParams<{ name: string }>();
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonMenuButton />
+          </IonButtons>
+          <IonTitle>Lojas</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">{name}</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <ExploreContainer name={name} />
+        {/* Include the Example component here */}
+        <Example />
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Lojas;
