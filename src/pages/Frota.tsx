04f9029d15edaafce5a92fe35741c322319a7d90
X-ERR0R-X <duartegomes@ipvc.pt>
2023-12-11 19:21:39 +0000

    .

--- src/pages/Frota.tsx
+++ src/pages/Frota.tsx
@@ -12,14 +12,106 @@ import {
   IonList,
   IonItem,
   IonLabel,
+  IonImg,
+  IonGrid,
+  IonRow,
+  IonCol,
 } from '@ionic/react';
 import { useParams } from 'react-router';
 import ExploreContainer from '../components/ExploreContainer';
 import './Frota.css';
 
+interface Car {
+  name: string;
+  description: string;
+  technicalData: string;
+  dailyRentPrice: string;
+}
+
 const Frota: React.FC = () => {
   const { name } = useParams<{ name: string }>();
   const [showModal, setShowModal] = useState(false);
+  const [selectedCar, setSelectedCar] = useState<Car | null>(null);
+
+  const cars: Car[] = [
+    {
+      name: 'Toyota Corolla',
+      description: 'Descrição do Toyota Corolla',
+      technicalData: 'Dados técnicos do Toyota Corolla',
+      dailyRentPrice: 'Preço do aluguer por dia do Toyota Corolla',
+    },
+    {
+      name: 'Honda Civic',
+      description: 'Descrição do Honda Civic',
+      technicalData: 'Dados técnicos do Honda Civic',
+      dailyRentPrice: 'Preço do aluguer por dia do Honda Civic',
+    },
+    {
+      name: 'Ford Focus',
+      description: 'Descrição do Ford Focus',
+      technicalData: 'Dados técnicos do Ford Focus',
+      dailyRentPrice: 'Preço do aluguer por dia do Ford Focus',
+    },
+    {
+      name: 'Chevrolet Cruze',
+      description: 'Descrição do Chevrolet Cruze',
+      technicalData: 'Dados técnicos do Chevrolet Cruze',
+      dailyRentPrice: 'Preço do aluguer por dia do Chevrolet Cruze',
+    },
+    {
+      name: 'Volkswagen Golf',
+      description: 'Descrição do Volkswagen Golf',
+      technicalData: 'Dados técnicos do Volkswagen Golf',
+      dailyRentPrice: 'Preço do aluguer por dia do Volkswagen Golf',
+    },
+    {
+      name: 'Nissan Altima',
+      description: 'Descrição do Nissan Altima',
+      technicalData: 'Dados técnicos do Nissan Altima',
+      dailyRentPrice: 'Preço do aluguer por dia do Nissan Altima',
+    },
+    {
+      name: 'Mazda CX-5',
+      description: 'Descrição do Mazda CX-5',
+      technicalData: 'Dados técnicos do Mazda CX-5',
+      dailyRentPrice: 'Preço do aluguer por dia do Mazda CX-5',
+    },
+    {
+      name: 'Subaru Outback',
+      description: 'Descrição do Subaru Outback',
+      technicalData: 'Dados técnicos do Subaru Outback',
+      dailyRentPrice: 'Preço do aluguer por dia do Subaru Outback',
+    },
+    {
+      name: 'Hyundai Sonata',
+      description: 'Descrição do Hyundai Sonata',
+      technicalData: 'Dados técnicos do Hyundai Sonata',
+      dailyRentPrice: 'Preço do aluguer por dia do Hyundai Sonata',
+    },
+    {
+      name: 'Kia Sportage',
+      description: 'Descrição do Kia Sportage',
+      technicalData: 'Dados técnicos do Kia Sportage',
+      dailyRentPrice: 'Preço do aluguer por dia do Kia Sportage',
+    },
+    {
+      name: 'Jeep Wrangler',
+      description: 'Descrição do Jeep Wrangler',
+      technicalData: 'Dados técnicos do Jeep Wrangler',
+      dailyRentPrice: 'Preço do aluguer por dia do Jeep Wrangler',
+    },
+
+  ];
+
+  const handleOpenModal = (car: Car) => {
+    setSelectedCar(car);
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setSelectedCar(null);
+  };
 
   return (
     <IonPage>
@@ -41,38 +133,47 @@ const Frota: React.FC = () => {
         <ExploreContainer name={name} />
 
         <IonList>
-          <IonItem>
-            <IonLabel>Toyota Corolla</IonLabel>
-            <IonButton onClick={() => setShowModal(true)}>Open Modal</IonButton>
-          </IonItem>
-          <IonItem>
-            <IonLabel>Opel Corsa</IonLabel>
-            <IonButton onClick={() => setShowModal(true)}>Open Modal</IonButton>
-          </IonItem>
-          <IonItem>
-            <IonLabel>Mercedes C200</IonLabel>
-            <IonButton onClick={() => setShowModal(true)}>Open Modal</IonButton>
-          </IonItem>
-          <IonItem>
-            <IonLabel>Fiat</IonLabel>
-            <IonButton onClick={() => setShowModal(true)}>Open Modal</IonButton>
-          </IonItem>
-          <IonItem>
-            <IonLabel>Mustang</IonLabel>
-            <IonButton onClick={() => setShowModal(true)}>Open Modal</IonButton>
-          </IonItem>
-          <IonItem>
-            <IonLabel>Renault</IonLabel>
-            <IonButton onClick={() => setShowModal(true)}>Open Modal</IonButton>
-          </IonItem>
+          {cars.map((car, index) => (
+            <IonItem key={index}>
+              <IonLabel>{car.name}</IonLabel>
+              <IonButton onClick={() => handleOpenModal(car)}>Mais informações</IonButton>
+            </IonItem>
+          ))}
         </IonList>
 
-        <IonModal isOpen={showModal} onDidDismiss={() => setShowModal(false)}>
-          <IonContent>
-            <IonTitle>Modal Content</IonTitle>
-            <IonButton onClick={() => setShowModal(false)}>Close Modal</IonButton>
-          </IonContent>
-        </IonModal>
+        {/* Modal */}
+        {selectedCar && (
+          <IonModal isOpen={showModal} onDidDismiss={handleCloseModal}>
+            <IonContent>
+              <IonTitle>{selectedCar.name}</IonTitle>
+
+              {/* Adicione aqui a imagem ilustrativa do carro */}
+              <IonImg src="caminho/para/imagem.jpg" />
+
+              {/* Informações do carro */}
+              <IonGrid>
+                <IonRow>
+                  <IonCol>
+                    <IonLabel>{selectedCar.description}</IonLabel>
+                  </IonCol>
+                </IonRow>
+                <IonRow>
+                  <IonCol>
+                    <IonLabel>{selectedCar.technicalData}</IonLabel>
+                  </IonCol>
+                </IonRow>
+                <IonRow>
+                  <IonCol>
+                    <IonLabel>{selectedCar.dailyRentPrice}</IonLabel>
+                  </IonCol>
+                </IonRow>
+              </IonGrid>
+
+              {/* Botão para fechar a modal */}
+              <IonButton onClick={handleCloseModal}>Fechar Modal</IonButton>
+            </IonContent>
+          </IonModal>
+        )}
       </IonContent>
     </IonPage>
   );
--- src/pages/Lojas.tsx
+++ src/pages/Lojas.tsx
@@ -12,15 +12,53 @@ import {
   IonList,
   IonItem,
   IonLabel,
-  IonImg,
+  IonGrid,
+  IonRow,
+  IonCol,
 } from '@ionic/react';
 import { useParams } from 'react-router';
 import ExploreContainer from '../components/ExploreContainer';
 import './Lojas.css';
 
+interface Store {
+  name: string;
+  description: string;
+  location: { latitude: number; longitude: number };
+}
+
 const Lojas: React.FC = () => {
   const { name } = useParams<{ name: string }>();
   const [showModal, setShowModal] = useState(false);
+  const [selectedStore, setSelectedStore] = useState<Store | null>(null);
+
+  const stores: Store[] = [
+    {
+      name: 'Loja A',
+      description: 'Descrição da Loja A',
+      location: { latitude: 40.7128, longitude: -74.0060 }, // Exemplo de coordenadas para Nova Iorque
+    },
+    {
+      name: 'Loja B',
+      description: 'Descrição da Loja B',
+      location: { latitude: 34.0522, longitude: -118.2437 }, // Exemplo de coordenadas para Los Angeles
+    },
+    {
+      name: 'Loja C',
+      description: 'Descrição da Loja C',
+      location: { latitude: 41.8781, longitude: -87.6298 }, // Exemplo de coordenadas para Chicago
+    },
+    // Adicione mais lojas conforme necessário
+  ];
+
+  const handleOpenModal = (store: Store) => {
+    setSelectedStore(store);
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setSelectedStore(null);
+  };
 
   return (
     <IonPage>
@@ -42,39 +80,44 @@ const Lojas: React.FC = () => {
         <ExploreContainer name={name} />
 
         <IonList>
-          <IonItem>
-            <IonLabel>Viana do castelo</IonLabel>
-            <IonButton onClick={() => setShowModal(true)}>Ver mais</IonButton>
-            <IonModal isOpen={showModal} onDidDismiss={() => setShowModal(false)}>
-          <IonContent>
-            <IonTitle></IonTitle>
-            <IonImg src="https://iili.io/EkXCvt.md.jpg" alt="Descrição da imagem" style={{ width: '100%', height: 'auto' }} />
-            <IonButton onClick={() => setShowModal(false)}>Fechar Modal</IonButton>
-          </IonContent>
-        </IonModal>
-          </IonItem>
-          
-          <IonItem>
-            <IonLabel>Braga</IonLabel>
-            <IonButton onClick={() => setShowModal(true)}>Ver mais </IonButton>
-            <IonModal isOpen={showModal} onDidDismiss={() => setShowModal(false)}>
-          <IonContent>
-            <IonTitle></IonTitle>
-            <IonImg src="https://iili.io/EkXCvt.md.jpg" alt="Descrição da imagem" style={{ width: '100%', height: 'auto' }} />
-            <IonButton onClick={() => setShowModal(false)}>Fechar Modal</IonButton>
-          </IonContent>
-        </IonModal>
-          </IonItem>
-          
+          {stores.map((store, index) => (
+            <IonItem key={index}>
+              <IonLabel>{store.name}</IonLabel>
+              <IonButton onClick={() => handleOpenModal(store)}>Mais informações</IonButton>
+            </IonItem>
+          ))}
         </IonList>
 
-        <IonModal isOpen={showModal} onDidDismiss={() => setShowModal(false)}>
-          <IonContent>
-            <IonTitle></IonTitle>
-            <IonImg src="https://iili.io/EkXCvt.md.jpg" alt="Descrição da imagem" style={{ width: '100%', height: 'auto' }} />
-            <IonButton onClick={() => setShowModal(false)}>Fechar Modal</IonButton>
-          </IonContent>
-        </IonModal>
+        {/* Modal */}
+        {selectedStore && (
+          <IonModal isOpen={showModal} onDidDismiss={handleCloseModal}>
+            <IonContent>
+              <IonTitle>{selectedStore.name}</IonTitle>
+
+              {/* Informações da loja */}
+              <IonGrid>
+                <IonRow>
+                  <IonCol>
+                    <IonLabel>{selectedStore.description}</IonLabel>
+                  </IonCol>
+                </IonRow>
+                <IonRow>
+                  <IonCol>
+                    <IonLabel>Latitude: {selectedStore.location.latitude}</IonLabel>
+                  </IonCol>
+                </IonRow>
+                <IonRow>
+                  <IonCol>
+                    <IonLabel>Longitude: {selectedStore.location.longitude}</IonLabel>
+                  </IonCol>
+                </IonRow>
+              </IonGrid>
+
+              {/* Botão para fechar a modal */}
+              <IonButton onClick={handleCloseModal}>Fechar Modal</IonButton>
+            </IonContent>
+          </IonModal>
+        )}
       </IonContent>
     </IonPage>
   );
